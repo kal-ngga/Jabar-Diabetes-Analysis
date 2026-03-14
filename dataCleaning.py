5ebc6085cf16b245905a4f95e3758c59992db8dc
kal-ngga <140575815+kal-ngga@users.noreply.github.com>
2026-03-14 11:45:22 +0700

    Uploading  Python, dataset, and visualitation result

---
+++ dataCleaning.py
@@ -0,0 +1,31 @@
+import pandas as pd
+
+df = pd.read_csv('dataset/Dataset TA1.csv')
+
+# 1. iformasi dan kondisi awal data
+print(f"Total baris awal: {df.shape[0]}")
+print("\njumlah data yang tidak null:")
+print(df.info())
+print("-" *40)
+print("\njumlah data yang null:")
+print(df.isnull().sum())
+print()
+print("=" *40)
+
+df_clean = df.dropna()
+numeric = ['Age', 'Hemoglobin', 'Hematokrit', 'Lekosit', 'Eritrosit', 'Trombosit', 'HbA1c', 'RBG']
+print(f"Total baris setelah drop null: {df_clean.shape[0]}")
+
+Q1 = df_clean.groupby('Diagnose')[numeric].transform(lambda x: x.quantile(0.25))
+Q3 = df_clean.groupby('Diagnose')[numeric].transform(lambda x: x.quantile(0.75))
+
+IQR = Q3 - Q1
+bawah = Q1 - 1.5 * IQR
+atas = Q3 + 1.5 * IQR
+
+outlier = (df_clean[numeric] >= bawah) & (df_clean[numeric] <= atas)
+baris_clean = outlier.all(axis=1)
+
+df_clean = df_clean[baris_clean]
+print(f"Total baris setelah drip outlier: {df_clean.shape[0]}")
+df_clean.to_csv("dataset/Dataset_Clean.csv", index=False)
---
+++ dataExploration.py
@@ -0,0 +1,75 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', 1000)
+
+df = pd.read_csv('dataset/Dataset_Clean.csv')
+
+# 2. Pengukuran statistik berdasarkan tipe data
+categorical = ['Gender', 'Diagnose']
+numeric = ['Age', 'Hemoglobin', 'Hematokrit', 'Lekosit', 'Eritrosit', 'Trombosit', 'HbA1c', 'RBG']
+
+print("\n--- FREKUENSI KATEGORIKAL ---")
+print(df[categorical].apply(pd.Series.value_counts).fillna(0))
+
+print("--- STATISTIK NUMERIK ---")
+print(f"\nJumlah Data (not null) - COUNT: {df[numeric].count()}")
+print(f"\nRata-rata - MEAN: {df[numeric].mean()}")
+print(f"\nStandar Deviasi - STD DEV: {df[numeric].std()}")
+print(f"\nNilai Terendah - MIN: {df[numeric].min()}")
+print(f"\nNilai Tengah - MEDIAN: {df[numeric].median()}")
+print(f"\nNilai Tertinggi - MAX: {df[numeric].max()}")
+print("=" *40)
+
+# 3. visualisasi keseluruhan data
+# A. Visualisasi Numerik
+df[numeric].hist(figsize=(15, 10), bins=20, edgecolor='black', grid=False)
+plt.suptitle("Histogram Variabel Numerik", fontsize=16)
+plt.tight_layout()
+plt.savefig('visualitation/Histogram_Numerik.png')
+
+# B. Visualisasi Kategorikal
+df[categorical].apply(pd.Series.value_counts).T.plot(kind='bar', figsize=(10, 5), cmap='coolwarm')
+plt.title("Bar Chart: Gender dan Diagnose")
+plt.xticks(rotation=0)
+plt.savefig('visualitation/BarChart_Kategori.png')
+
+# 4. visualisasi data diagnosis
+df_melt = df.melt(id_vars='Diagnose', value_vars=numeric)
+sns.catplot(
+    data=df_melt, x='Diagnose', y='value',
+    col='variable', col_wrap=4,
+    kind='box', sharey=False, height=4, aspect=1.2
+)
+plt.suptitle("Boxplot Numerik Berdasarkan Diagnosis", y=1.02, fontsize=16)
+plt.savefig('visualitation/Boxplot_Diagnosis.png')
+
+# 4. pengujian korelasui Pearson
+print("\n--- KORELASI PEARSON ---")
+corr_matrix = df[numeric].corr(method='pearson')
+print(corr_matrix)
+
+# heatmap korelasi
+plt.figure(figsize=(10, 8))
+sns.heatmap(corr_matrix, annot=True, cmap='coolwarm', fmt=".2f", linewidths=0.5)
+plt.title("Pearson Correlation Heatmap")
+plt.tight_layout()
+plt.savefig('visualitation/Heatmap_Korelasi.png')
+
+# 6. menghitung dan mendeteksi outlier
+print("\n--- DETEKSI OUTLIER (Jumlah Outlier per Variabel) ---")
+# Menghitung Q1 dan Q3
+Q1 = df.groupby('Diagnose')[numeric].transform(lambda x: x.quantile(0.25))
+Q3 = df.groupby('Diagnose')[numeric].transform(lambda x: x.quantile(0.75))
+
+# Rumus IQR & Batas
+IQR = Q3 - Q1
+bawah = Q1 - 1.5 * IQR
+atas = Q3 + 1.5 * IQR
+
+outlier = (df[numeric] >= bawah) & (df[numeric] <= atas)
+outlier_counts = outlier.groupby(df['Diagnose']).sum()
+print(outlier_counts)
